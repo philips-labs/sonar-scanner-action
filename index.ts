7a75af86e2efb28d0ed32b097028d72e394efcdf
Patrick Mennen <patrick.mennen@philips.com>
2020-01-31 06:24:50 +0100

    add await in async root function

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import * as core from '@actions/core';
 
 async function run(): Promise<void> {
   try {
-    sonarScanner();
+    await sonarScanner();
   } catch (error) {
     core.setFailed(error.message);
   }
