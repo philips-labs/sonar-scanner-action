5e4e98c9d46bbcfa4023cb6ae38f777c7eab72e8
Patrick Mennen <patrick.mennen@philips.com>
2020-04-14 15:19:40 +0200

    fixes single test-file instead of multiple test files

--- src/sonarScanner.test.ts
+++ src/sonarScanner.test.ts
@@ -5,14 +5,23 @@ jest.mock('@actions/exec');
 jest.mock('@actions/github', () => ({
   ...jest.requireActual('@actions/github'),
   context: {
-    payload: {},
+    payload: {
+      pull_request: {
+        number: 101,
+        base: {
+          ref: 'master',
+        },
+        head: {
+          ref: 'feature/featureX',
+        },
+      },
+    },
   },
 }));
 
 describe('SonarQube Scanner Action', () => {
   beforeEach(() => {
     jest.resetAllMocks();
-
     process.env['INPUT_PROJECTNAME'] = 'HelloWorld';
     process.env['INPUT_PROJECTKEY'] = 'key';
     process.env['INPUT_BASEDIR'] = 'src/';
@@ -71,5 +80,51 @@ describe('SonarQube Scanner Action', () => {
     } catch (e) {
       expect(e.message).toBe('SonarScanner failed');
     }
+  });
+
+  describe('Pull Request', () => {
+    beforeEach(() => {
+      jest.resetModules();
+      process.env['INPUT_PROJECTNAME'] = 'HelloWorld';
+      process.env['INPUT_PROJECTKEY'] = 'key';
+      process.env['INPUT_BASEDIR'] = '.';
+      process.env['INPUT_TOKEN'] = 'Dummy-Security-Token';
+      process.env['INPUT_URL'] = 'http://example.com';
+      process.env['INPUT_SCMPROVIDER'] = 'git';
+      process.env['INPUT_SOURCEENCODING'] = 'UTF-8';
+    });
+
+    it('starts the action for pull request decoration.', async () => {
+      process.env['INPUT_ENABLEPULLREQUESTDECORATION'] = 'true';
+
+      await sonarScanner();
+      expect(exec).toHaveBeenCalledWith('sonar-scanner', [
+        '-Dsonar.login=Dummy-Security-Token',
+        '-Dsonar.host.url=http://example.com',
+        '-Dsonar.projectBaseDir=.',
+        '-Dsonar.projectKey=key',
+        '-Dsonar.projectName=HelloWorld',
+        '-Dsonar.scm.provider=git',
+        '-Dsonar.sourceEncoding=UTF-8',
+        '-Dsonar.pullrequest.key=101',
+        '-Dsonar.pullrequest.base=master',
+        '-Dsonar.pullrequest.branch=feature/featureX',
+      ]);
+    });
+
+    it('starts the action for pull request without decoration.', async () => {
+      process.env['INPUT_ENABLEPULLREQUESTDECORATION'] = 'false';
+
+      await sonarScanner();
+      expect(exec).toHaveBeenCalledWith('sonar-scanner', [
+        '-Dsonar.login=Dummy-Security-Token',
+        '-Dsonar.host.url=http://example.com',
+        '-Dsonar.projectBaseDir=.',
+        '-Dsonar.projectKey=key',
+        '-Dsonar.projectName=HelloWorld',
+        '-Dsonar.scm.provider=git',
+        '-Dsonar.sourceEncoding=UTF-8',
+      ]);
+    });
   });
 });
--- src/sonarScanner.ts
+++ src/sonarScanner.ts
@@ -2,23 +2,6 @@ import * as core from '@actions/core';
 import { context } from '@actions/github';
 import { exec } from '@actions/exec';
 
-jest.mock('@actions/github', () => ({
-  ...jest.requireActual('@actions/github'),
-  context: {
-    payload: {
-      pull_request: {
-        number: 101,
-        base: {
-          ref: 'master',
-        },
-        head: {
-          ref: 'feature/featureX',
-        },
-      },
-    },
-  },
-}));
-
 export const sonarScanner = async () => {
   const projectName = core.getInput('projectName', { required: true });
   const projectKey = core.getInput('projectKey', { required: true });
@@ -55,6 +38,7 @@ export const sonarScanner = async () => {
   `);
 
   const pr: any = context.payload.pull_request;
+
   if (enablePullRequestDecoration && pr) {
     core.info(`
     -- Configuration for pull request decoration:
@@ -81,48 +65,3 @@ export const sonarScanner = async () => {
 
   core.endGroup();
 };
-
-describe('SonarQube Scanner Action for a Pull Request', () => {
-  beforeEach(() => {
-    process.env['INPUT_PROJECTNAME'] = 'HelloWorld';
-    process.env['INPUT_PROJECTKEY'] = 'key';
-    process.env['INPUT_BASEDIR'] = '.';
-    process.env['INPUT_TOKEN'] = 'Dummy-Security-Token';
-    process.env['INPUT_URL'] = 'http://example.com';
-    process.env['INPUT_SCMPROVIDER'] = 'git';
-    process.env['INPUT_SOURCEENCODING'] = 'UTF-8';
-  });
-
-  it('starts the action for pull request decoration.', async () => {
-    process.env['INPUT_ENABLEPULLREQUESTDECORATION'] = 'true';
-
-    await sonarScanner();
-    expect(exec).toHaveBeenCalledWith('sonar-scanner', [
-      '-Dsonar.login=Dummy-Security-Token',
-      '-Dsonar.host.url=http://example.com',
-      '-Dsonar.projectBaseDir=.',
-      '-Dsonar.projectKey=key',
-      '-Dsonar.projectName=HelloWorld',
-      '-Dsonar.scm.provider=git',
-      '-Dsonar.sourceEncoding=UTF-8',
-      '-Dsonar.pullrequest.key=101',
-      '-Dsonar.pullrequest.base=master',
-      '-Dsonar.pullrequest.branch=feature/featureX',
-    ]);
-  });
-
-  it('starts the action for pull request without decoration.', async () => {
-    process.env['INPUT_ENABLEPULLREQUESTDECORATION'] = 'false';
-
-    await sonarScanner();
-    expect(exec).toHaveBeenCalledWith('sonar-scanner', [
-      '-Dsonar.login=Dummy-Security-Token',
-      '-Dsonar.host.url=http://example.com',
-      '-Dsonar.projectBaseDir=.',
-      '-Dsonar.projectKey=key',
-      '-Dsonar.projectName=HelloWorld',
-      '-Dsonar.scm.provider=git',
-      '-Dsonar.sourceEncoding=UTF-8',
-    ]);
-  });
-});
